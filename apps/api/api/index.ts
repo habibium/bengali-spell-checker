dc5ef34393f4a89dde42898e24263e47b8e3dc5b
habibium <habib.prodev@gmail.com>
2025-07-26 20:52:17 +0600

    chore: update API to include CORS support and improve code formatting

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,14 +1,24 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { handle } from "hono/vercel";
 
 export const config = {
-  runtime: 'edge'
-}
+  runtime: "edge",
+};
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
+app.use(
+  cors({
+    origin: ["https://hoppscotch.io"],
+  })
+);
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-})
+app.get("/", (c) => {
+  return c.json({ message: "Hello Hono!" });
+});
 
-export default handle(app)
+app.get("/:message", (c) => {
+  return c.json({ message: `Hello ${c.req.param("message")}` });
+});
+
+export default handle(app);
